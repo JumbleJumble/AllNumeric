921f338a9075e90fa9db24311e227c514c825d97
Richard Irons <richard.irons@five.ai>
2019-10-28 22:10:20 +0000

    Implemented two ways

---
+++ Student/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            char[] charArray = Console.ReadLine().ToCharArray();
+            if (IsCharArrayNumeric(charArray))
+            {
+                Console.WriteLine("All characters are numeric");
+            }
+            else
+            {
+                Console.WriteLine("Array contains non-numeric characters");
+            }
+        }
+
+        private static bool IsCharArrayNumeric(char[] charArray)
+        {
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (!char.IsNumber(charArray[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Veteran/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Veteran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string enteredString = Console.ReadLine();
+            bool allNumeric = AllNumeric(enteredString);
+            
+            string message = allNumeric
+                ? "All characters are numeric"
+                : "Array contains non-numeric characters";
+
+            Console.WriteLine(message);
+        }
+
+        private static bool AllNumeric(string enteredString)
+        {
+            return enteredString.All(char.IsNumber);
+        }
+    }
+}
